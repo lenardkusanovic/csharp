924190ff162e0dbd7933493e9e44bb5be7fa8969
lenardkusanovic <55271599+lenardkusanovic@users.noreply.github.com>
2019-09-24 11:32:27 +0200

    Add files via upload

---
+++ kusanovicLenardMojaPrvaAplikacija/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kusanovicLenardMojaPrvaAplikacija
+{
+    class Program
+    {
+        private static object console;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello world");
+
+            Console.ReadKey();
+
+        }
+    }
+}
