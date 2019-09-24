2f5caef648020cfd8bcef79ba752644d4ac2f21c
lenardkusanovic <55271599+lenardkusanovic@users.noreply.github.com>
2019-09-24 12:12:46 +0200

    Add files via upload

---
+++ kusanovicLenardMojaDrugaAplikacija/kusanovicLenardMojaDrugaAplikacija/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kusanovicLenardMojaDrugaAplikacija
+{
+    public partial class Form1 : Form
+    {
+        int broj = 0;
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            broj++;
+            txtPrikaz.Text = broj.ToString();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            broj--;
+            txtPrikaz.Text = broj.ToString();
+        }
+    }
+}
